377230a937a83d1c087bfc4a4a08eda1214ee599
Daniel King <dan@spiraldb.com>
2025-12-05 12:09:28 -0500

    fix typing
    Signed-off-by: Daniel King <dan@spiraldb.com>

--- src/python.rs
+++ src/python.rs
@@ -258,7 +258,7 @@ impl PyVortexCodec {
         // SAFETY: ReadOnlyCell is transparent, UnsafeCell has the same memory
         // layout. If Python mutates this concurrently, we're dead.
         let slice: &[u8] = unsafe { std::mem::transmute(slice) };
-        let vortex_bytes = Buffer::from(Bytes::from_owner(slice));
+        let vortex_bytes = Buffer::<T>::from_byte_buffer(Buffer::from(Bytes::from_owner(slice)));
         // TODO(DK): Can we get invalidity information from Zarr?
         let vortex_array = PrimitiveArray::new(vortex_bytes, Validity::AllValid);
 
